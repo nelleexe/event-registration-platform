5143e23ad1a0429b51803074bb5ba80f8af9f5db
rick4lderson <61159927+MichaelBogdanov@users.noreply.github.com>
2026-04-07 21:59:58 +0300

    Подключены иконки и добавлены мелкие визуальные улучшения

--- project/website/templatetags/user_filters.py
+++ project/website/templatetags/user_filters.py
@@ -11,3 +11,12 @@ def get_user_events(user):
 @register.filter
 def get_user_clubs(user):
     return [i.event for i in EventMember.objects.filter(user=user, event__club__isnull=False)]
+
+@register.filter
+def get_role(user):
+    ROLES = {
+        'student': 'Учащийся',
+        'parent': 'Родитель',
+        'organizer': 'Организатор'
+    }
+    return ROLES[user.role]
